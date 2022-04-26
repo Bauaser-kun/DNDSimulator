4bdedbd9cc6837439668db3e6e1b427f1a89d08f
mbalattpsc <michal.bala@ttpsc.pl>
2022-04-26 13:25:25 +0200

    Race fields added

--- src/main/java/dnd/simulator/Race.java
+++ src/main/java/dnd/simulator/Race.java
@@ -1,5 +1,16 @@
 package dnd.simulator;
 
 public class Race {
-    
+    public String name;
+    public String type;
+    public String subtype;
+    public List modifiers;
+    public List<String> racialFeats;
+    public List<String> racialSpellLikes;
+    public List<String> racialExtraordinnary;
+    public List<String> racialSuperNatural;
+    public DamageResistance damageResistance;
+    public SpellResistance spellResistance;
+    public FastHealing fastHealing;
+    public Regeneration regeneration;
 }
