254710734404ff56daf4a2e34d5b0fc244d01732
mbalattpsc <michal.bala@ttpsc.pl>
2023-09-19 11:56:26 +0200

    separated Roll Assigner as separate class, changed accessability fields for Creature to private + relevant code changes (getters/setters instead of dot operator etc

--- src/main/java/dnd/simulator/Creatures/Creature.java
+++ src/main/java/dnd/simulator/Creatures/Creature.java
@@ -10,15 +10,88 @@ public class Creature {
     @Id
     String species;
     
-    String type;
-    String subtype;
-    String size;
-    String speed;
-    String characterClass;
-    int strength;
-    int dexterity;
-    int constitution;
-    int intelligence;
-    int wisdom;
-    int charisma;
+    private String type;
+    private String subtype;
+    private String size;
+    private String speed;
+    private String characterClass;
+    private int strength;
+    private int dexterity;
+    private int constitution;
+    private int intelligence;
+    private int wisdom;
+    private int charisma;
+
+    public String getSpecies() {
+        return species;
+    }
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    public String getSubtype() {
+        return subtype;
+    }
+    public void setSubtype(String subtype) {
+        this.subtype = subtype;
+    }
+    public String getSize() {
+        return size;
+    }
+    public void setSize(String size) {
+        this.size = size;
+    }
+    public String getSpeed() {
+        return speed;
+    }
+    public void setSpeed(String speed) {
+        this.speed = speed;
+    }
+    public String getCharacterClass() {
+        return characterClass;
+    }
+    public void setCharacterClass(String characterClass) {
+        this.characterClass = characterClass;
+    }
+    public int getStrength() {
+        return strength;
+    }
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+    public int getDexterity() {
+        return dexterity;
+    }
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+    public int getConstitution() {
+        return constitution;
+    }
+    public void setConstitution(int constitution) {
+        this.constitution = constitution;
+    }
+    public int getIntelligence() {
+        return intelligence;
+    }
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+    public int getWisdom() {
+        return wisdom;
+    }
+    public void setWisdom(int wisdom) {
+        this.wisdom = wisdom;
+    }
+    public int getCharisma() {
+        return charisma;
+    }
+    public void setCharisma(int charisma) {
+        this.charisma = charisma;
+    }
 }
---
+++ src/main/java/dnd/simulator/Creatures/supportCases/rolls/RollAssigner.java
@@ -0,0 +1,87 @@
+package dnd.simulator.creatures.supportCases.rolls;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import dnd.simulator.creatures.Creature;
+import dnd.simulator.creatures.supportCases.prioritizers.AtributesPrioritizer;
+import io.micrometer.common.lang.Nullable;
+
+public class RollAssigner {
+    AtributesPrioritizer atributesPrioritizer = new AtributesPrioritizer();
+
+    public Creature assignRolls(Creature creature, @Nullable String className, String rollsUsed, String role) {
+        List<Integer> atributeValues = new ArrayList<>();
+        List<String> prioritizedAtributes = new ArrayList<>();
+        
+        Map<String, Integer> assignedRolls = new HashMap<>();
+        assignedRolls.put("strength", null);
+        assignedRolls.put("constitution", null);
+        assignedRolls.put("dexterity", null);
+        assignedRolls.put("intelligence", null);
+        assignedRolls.put("wisdom", null);
+        assignedRolls.put("charisma", null);
+
+        switch (rollsUsed) {
+            case "basic": 
+                atributeValues.add(BasicRolls.HIGHEEST_ROLL.getRoll());
+                atributeValues.add(BasicRolls.VERY_HIGH_ROLL.getRoll());
+                atributeValues.add(BasicRolls.HIGH_ROLL.getRoll());
+                atributeValues.add(BasicRolls.LOW_ROLL.getRoll());
+                atributeValues.add(BasicRolls.VERY_LOW_ROLL.getRoll());
+                atributeValues.add(BasicRolls.LOWEST_ROLL.getRoll());
+                break;
+            case "good":
+                atributeValues.add(GoodRolls.HIGHEEST_ROLL.getRoll());
+                atributeValues.add(GoodRolls.VERY_HIGH_ROLL.getRoll());
+                atributeValues.add(GoodRolls.HIGH_ROLL.getRoll());
+                atributeValues.add(GoodRolls.LOW_ROLL.getRoll());
+                atributeValues.add(GoodRolls.VERY_LOW_ROLL.getRoll());
+                atributeValues.add(GoodRolls.LOWEST_ROLL.getRoll());
+                break;
+            case "elite":
+                atributeValues.add(EliteRolls.HIGHEEST_ROLL.getRoll());
+                atributeValues.add(EliteRolls.VERY_HIGH_ROLL.getRoll());
+                atributeValues.add(EliteRolls.HIGH_ROLL.getRoll());
+                atributeValues.add(EliteRolls.LOW_ROLL.getRoll());
+                atributeValues.add(EliteRolls.VERY_LOW_ROLL.getRoll());
+                atributeValues.add(EliteRolls.LOWEST_ROLL.getRoll());                
+                break;
+            case "maxed":
+                for (int i = 0; i < 6; i++) {
+                    atributeValues.add(18);
+                }
+                break;
+            case "custom":
+                break;
+            default:
+                 for (int i = 0; i < 3; i++) {
+                    atributeValues.add(11);
+                }
+                 for (int i = 0; i < 3; i++) {
+                    atributeValues.add(10);
+                }
+        }
+
+        if (className != null ) {
+            prioritizedAtributes = atributesPrioritizer.prioritizeAtributes(role, className);
+        } else {
+            prioritizedAtributes = atributesPrioritizer.prioritizeAtributes(role, creature.getType());
+        }
+
+        for (int i = 0; i < 6; i++) {
+            assignedRolls.put(prioritizedAtributes.get(i), atributeValues.get(i));
+        }
+      
+        creature.setStrength(assignedRolls.get("strength"));
+        creature.setConstitution(assignedRolls.get("constitution"));
+        creature.setDexterity(assignedRolls.get("dexterity"));
+        creature.setIntelligence(assignedRolls.get("intelligence"));
+        creature.setWisdom(assignedRolls.get("wisdom"));
+        creature.setCharisma(assignedRolls.get("charisma"));
+
+        return creature;
+    }
+}
