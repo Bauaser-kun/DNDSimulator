48eafa99ce64018108a0c4fede9b751d09987635
mbalattpsc <michal.bala@ttpsc.pl>
2023-10-04 14:00:34 +0200

    minor css modification

--- src/main/resources/testQueries.js
+++ src/main/resources/testQueries.js
@@ -9,16 +9,19 @@ async function populateTableWithPulledData(url, table) {
     tableHead.innerHTML = "<tr></tr>";
     tableBody.innerHTML = "";
 
+    //Assign headers
     for (const column of columns) {
         const headerElement = document.createElement("th");
         headerElement.textContent = column;
         tableHead.querySelector("tr").appendChild(headerElement);
     }
 
+    //assign rows
     for (let i = 0; i < data.length; i++) {
         const bodyRow = document.createElement("tr");
         const row = Object.values(data[i]);
 
+        //assign cells
         for (const cell of row) {
             const cellElement = document.createElement("td");
             cellElement.textContent = cell;
