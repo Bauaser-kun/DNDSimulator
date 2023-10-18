13a346be433e3775986efaf0696755cb698e78cb
mbalattpsc <michal.bala@ttpsc.pl>
2023-10-18 15:11:45 +0200

    initial implementation

--- src/main/java/dnd/simulator/Creatures/supportCases/skills/Skill.java
+++ src/main/java/dnd/simulator/Creatures/supportCases/skills/Skill.java
@@ -1,5 +1,29 @@
 package dnd.simulator.creatures.supportCases.skills;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "skills")
 public class Skill {
+    @Id
+    @Column(name = "skill")
+    private String skillName;
     
+    @Column(name =  "rank")
+    private double rank;
+
+    @Column(name = "maxRank")
+    private double maxRank;
+
+    @Column(name = "atribute")
+    private String revelantAtribute;
 }
--- src/main/java/dnd/simulator/Creatures/supportCases/skills/SkillDto.java
+++ src/main/java/dnd/simulator/Creatures/supportCases/skills/SkillDto.java
@@ -1,5 +1,17 @@
 package dnd.simulator.creatures.supportCases.skills;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class SkillDto {
-    
+    private String skillName;
+    private double rank;
+    private double maxRank;
+    private String revelantAtribute;
 }
