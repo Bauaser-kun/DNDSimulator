6cf9449bdd65a8c3ef3a9150ebfc7f5e66a3e5d8
Bauaser-kun <michal.bala.1980@gmail.com>
2023-12-28 14:53:46 +0100

    updating after coming back to project

--- Tests/src/test/java/dnd/simulator/database/ClassRepositoryTestSuite.java
+++ Tests/src/test/java/dnd/simulator/database/ClassRepositoryTestSuite.java
@@ -7,11 +7,13 @@ import java.util.*;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import dnd.simulator.creatures.CharacterClass;
 
 @RunWith(SpringRunner.class)
+//@SpringBootTest
 public class ClassRepositoryTestSuite {
     @Autowired
     DBService dbService;
